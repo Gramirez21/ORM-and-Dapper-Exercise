c6304892a221a018bc4bab09b05da4c6de231c56
Gramirez21 <r.dannysixty@gmail.com>
2024-04-22 22:13:19 -0700

    orm

---
+++ ORM-Dapper/DapperProductRepositorycs.cs
@@ -0,0 +1,33 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ORM_Dapper
+{
+    public class DapperProductRepositorycs : IProductRepsotory
+    {
+        private readonly IDbConnection _conn;
+
+        public DapperProductRepositorycs(IDbConnection conn)
+        {
+            _conn = conn;
+        }
+
+        public void CreateProduct(string name, double price, int categoryID)
+        {
+            _conn.Execute("INSERT INTO products (name) VALUES (@name) (Price) VALUES (@price) (CategoryId) VALUES (@categoryId",
+                new { name = name, price = price, categoryId = categoryID});
+        }
+
+        public IEnumerable<Product> GetAllProducts()
+        {
+            return _conn.Query<Product>("SELECT * FROM Products;");
+        }
+
+        
+    }
+}
--- ORM-Dapper/Program.cs
+++ ORM-Dapper/Program.cs
@@ -1,10 +1,53 @@
-﻿namespace ORM_Dapper
+﻿using Microsoft.Extensions.Configuration;
+using MySql.Data.MySqlClient;
+using System.Data;
+
+namespace ORM_Dapper
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            var config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsetting.json")
+                .Build();
+
+            string connString = config.GetConnectionString("DefaultConnection");
+
+            IDbConnection conn = new MySqlConnection(connString);
+
+
+            var departmentRepository = new DapperDepartmentRepository(conn);
+
+            var departments = departmentRepository.GetDepartments();
+
+            foreach (var department in departments)
+            {
+                Console.WriteLine(department.Name);
+                Console.WriteLine(department.DepartmentID);
+                Console.WriteLine();
+            }
+
+            var productRepository = new DapperProductRepositorycs(conn);
+
+
+
+            var products = productRepository.GetAllProducts();
+
+            foreach (var product in products)
+            {
+                Console.WriteLine(product.Name);
+                Console.WriteLine(product.ProductID);
+                Console.WriteLine(product.Price);
+                Console.WriteLine(product.CategoryID);
+                Console.WriteLine(product.StockLevel);
+                Console.WriteLine(product.OnSale);
+                Console.WriteLine();
+
+            }
+
         }
     }
 }
